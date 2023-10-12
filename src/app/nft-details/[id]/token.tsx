2a5fbbf1e82975678915846e0d9d53cdfdc9fad7
Akshit Ostwal <akshitostwal@gmail.com>
2023-10-13 01:30:12 +0530

    fix infinite fetchData call

--- src/app/nft-details/[id]/token.tsx
+++ src/app/nft-details/[id]/token.tsx
@@ -4,7 +4,7 @@ import { Token } from '@/models/models';
 import axios from 'axios';
 import { ethers } from 'ethers';
 import { default as NXTImage } from "next/image";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 const BUTTON_CLASS = `text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`;
@@ -41,7 +41,10 @@ function TokenLayout(props: TokenLayoutProps) {
         }
     };
 
-    fetchData(props.id);
+
+    useEffect(() => {
+        fetchData(props.id);
+    }, []);
 
 
     const handleRefreshClick = () => {
